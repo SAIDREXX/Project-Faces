e58fe07518d72ed2d87eba2e8ac520417dc782e2
Rafael Said Hernandez Demeneghi <saidrexxdemher@gmail.com>
2024-05-13 19:22:12 -0600

    Tailwind Added & Configured

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				Primary01: "#233862",
+				Primary02: "#0C4CBA",
+				Secondary01: "#75B2E1",
+				Accent01: "#173C86",
+				BackgroundLight: "DBDBDB",
+				BackgroundDark: "232323",
+				FontLight: "232323",
+				FontDark: "DBDBDB",
+			},
+			fontFamily: {
+				Inter: ["Inter Variable", "sans-serif"],
+			}
+		},
 	},
 	plugins: [],
 }
